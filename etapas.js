95e3886e575563e0baa02e538c04f9d5de593f98
Vinicius Gonçalves <vinicius_k.o@hotmail.com>
2022-07-10 01:46:16 -0300

    init back-end
    with JS

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 33333,
+                nome:'Nicolas Jr',
+                partido:'warner',
+                fotos:[
+                    {url:'33333.jpg', legenda:'Vereador'}
+                ]
+            },
+            {
+                numero:22222,
+                nome:'Nicolas Beltrano',
+                partido:'DIF',
+                fotos:[
+                    {url:'22222.jpg', legenda:'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos: [
+            {
+                numero:22,
+                nome:'Nicolas Silva',
+                partido:'VERDE',
+                vice:'Niquinho Cage',
+                fotos:[
+                    {url:'22.jpg', legenda:'Prefeito'},
+                    {url:'23.jpg', legenda:'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero:20,
+                nome:'Nicolas Skynet',
+                partido:'Machine Learning',
+                vice:'John Connor',
+                fotos:[
+                    {url:'22.jpg', legenda:'Prefeito'},
+                    {url:'23.jpg', legenda:'Vice-Prefeito'}
+                ]
+            },
+        ]
+    }
+]
