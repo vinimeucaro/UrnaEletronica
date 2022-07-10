21071654f8e0bef0590b14a6a5f5a81019c3aa77
Vinicius Gonçalves <vinicius_k.o@hotmail.com>
2022-07-10 02:13:15 -0300

    control and click

---
+++ script.js
@@ -0,0 +1,6 @@
+let SeuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.base');
+let lateral = document.querySelector('topo--direita');
+let lateral = document.querySelector('d-1-3');
